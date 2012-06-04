5e99aea0fe711c6dc541616d9b230ca0df5bcabe
mismo <mismo@yeah.net>
2012-06-04 09:36:47 +0800

    add phantomjs

--- src/web/lib/jasmine/go.js
+++ src/web/lib/jasmine/go.js
@@ -2,6 +2,6 @@ if ('undefined' != typeof isHtml) {
 	jasmine.getEnv().addReporter(new jasmine.ConsoleReporter());
 	jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
 } else {
-	jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('./report/'));
+	jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('./build/junitReport/'));
 }
 jasmine.getEnv().execute();
